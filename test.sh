c855e6bf2e778f818c1df431870aff4e12b6c66a
Tim Dévé <TimDeve@users.noreply.github.com>
2021-03-08 16:55:56 +0000

    Flattens DSL submodules

--- test.sh
+++ test.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+echo "Building example..."
+carp -b example.carp
+
+echo "Running tests..."
 for f in ./test/*.carp; do
     echo $f
     carp -x --log-memory $f
     echo
 done
+
