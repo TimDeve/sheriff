d96b2cef74316a1507b07fdf8c3d1b979a3e8b83
Tim Dévé <TimDeve@users.noreply.github.com>
2022-10-10 12:20:57 +0100

    Parses negative numbers
    Can't believe if forgot that...

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+if [[ "$(uname)" =~ "Darwin" ]]; then
+  export MallocNanoZone=0 # Removes error when using ASAN on MacOs
+fi
+
 echo "Running tests..."
 for f in ./tests/*.carp; do
   echo
